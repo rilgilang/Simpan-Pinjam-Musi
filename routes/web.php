d115e18a8420b3315050a851556914bf16b68d41
gilang <gilang.sukma@digitalsekuriti.id>
2025-06-11 16:19:20 +0700

    added anggota list table

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AnggotaController;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
 
 Route::view('/', 'welcome');
 
@@ -11,5 +14,9 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::group(['middleware' => ['role:admin']], function () {
+    Route::view('/anggota', "anggota-list");
+});
 
 require __DIR__.'/auth.php';
